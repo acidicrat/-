a65e6df02cd80ecc9f08a2468e8560569462a6a4
acidicrat <48647971+acidicrat@users.noreply.github.com>
2019-05-19 17:09:36 +0300

    Add files via upload

--- Лабораторная работа 2/lab2zadanie1.py
+++ Лабораторная работа 2/lab2zadanie1.py
@@ -1,5 +1,5 @@
-slovo = "Митяй, литий, Кирилл, картошечка, лицо, Литва, Рыжик, Ликёр"
+slovo = "Митяй, литий, Кирилл, картошечка,Л,ли, лицо, Литва, Рыжик, Ликёр"
 slovo = slovo.split(', ')
 for i in range(0, len(slovo)):
-    if slovo[i][0] == 'Л' and slovo[i][1] == 'и':
+    if len(slovo) >= 2 and slovo[i][0] == 'Л' and slovo[i][1] == 'и':
         print(slovo[i])
